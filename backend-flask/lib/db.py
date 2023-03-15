0677efe58023b1232304296a7f254106f00e04f8
Sourav Kumar <souravk326@gmail.com>
2023-03-15 13:21:47 +0000

    sql connection

---
+++ backend-flask/lib/db.py
@@ -0,0 +1,24 @@
+from psycopg_pool import ConnectionPool
+import os
+import re
+import sys
+from flask import current_app as app
+
+def query_wrap_object(template):
+  sql = f"""
+  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+  {template}
+  ) object_row);
+  """
+  return sql
+
+def query_wrap_array(template):
+  sql = f"""
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+  {template}
+  ) array_row);
+  """
+  return sql
+  
+connection_url = os.getenv("CONNECTION_URL")
+pool = ConnectionPool(connection_url)
