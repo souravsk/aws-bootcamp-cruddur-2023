084113972e4d9bbc85a4a16c7d0c5a4f66d05710
Sourav Kumar <souravk326@gmail.com>
2023-03-18 10:29:55 +0000

    it connected with postgres

--- aws/lambdas/cruddur-post-confirrmation.py
+++ aws/lambdas/cruddur-post-confirrmation.py
@@ -2,45 +2,42 @@ import json
 import psycopg2
 import os
 
+
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
-    print('userAttributes')
-    print(user)
-
-    user_display_name  = user['name']
-    user_email         = user['email']
-    user_handle        = user['preferred_username']
-    user_cognito_id    = user['sub']
-
-    conn = None
+    user_display_name = user['name']
+    user_email = user['email']
+    user_handle = user['preferred_username']
+    user_cognito_id = user['sub']
+    
     try:
-        print('entered-try')
-        sql = f"""
+        sql = """
             INSERT INTO public.users (
                 display_name, 
                 email,
                 handle, 
                 cognito_user_id
             ) 
-            VALUES(
-                '{user_display_name}', 
-                '{user_email}', 
-                '{user_handle}', 
-                '{user_cognito_id}'
-            )
+            VALUES (%s, %s, %s, %s)
         """
-        print('SQL Statement ----')
-        print(sql)
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit() 
+        
+        params = [
+            user_display_name,
+            user_email,
+            user_handle,
+            user_cognito_id
+        ]
+        
+        with psycopg2.connect(os.getenv('CONNECTION_URL')) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, params)
+            conn.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+    else:
+        print("Data inserted successfully")
     finally:
-        if conn is not None:
-            cur.close()
-            conn.close()
-            print('Database connection closed.')
+        print('Database connection closed.')
+    
     return event
