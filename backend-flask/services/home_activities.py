95d1bcd898e32a18dc2b2de49f21a261407cd38b
Sourav Kumar <souravk326@gmail.com>
2023-03-18 10:30:41 +0000

    make the create_activity working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -8,24 +8,10 @@ from lib.db import db
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    # with tracer.start_as_current_span("home-activites-mock-data"):
-    #   span = trace.get_current_span()
-    #   now = datetime.now(timezone.utc).astimezone()
-    #   span.set_attribute("app.now", now.isoformat())
-    results = db.query_array_json("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
     return results
