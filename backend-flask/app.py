d01b3436050fc727b7d80e8b73e89ce08a3e011d
Sourav Kumar <souravk326@gmail.com>
2023-03-22 09:30:42 +0000

    change user handle name

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -198,7 +198,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'sovu'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
