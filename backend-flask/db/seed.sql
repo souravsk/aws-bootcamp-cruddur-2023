578c12111552d4450bc78b2cda141681de225a69
Sourav Kumar <souravk326@gmail.com>
2023-03-24 10:16:45 +0000

    change the user data form andrew to mine

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.com','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.com', 'bayko' ,'MOCK');
+  ('Sourav Kumar','souravk326@gmail.com','sovu' ,'MOCK'),
+  ('Son Goku','songoku02010@gmail.com', 'goku' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'sovu' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
