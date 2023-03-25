6bf0772877b06a98dede5ae93eb90cc714dc89d5
Sourav Kumar <souravk326@gmail.com>
2023-03-25 07:48:30 +0000

    handle name change

--- backend-flask/services/create_message.py
+++ backend-flask/services/create_message.py
@@ -31,7 +31,7 @@ class CreateMessage:
     if model['errors']:
       # return what we provided
       model['data'] = {
-        'display_name': 'Andrew Brown',
+        'display_name': 'Sourav Kumar',
         'handle':  user_sender_handle,
         'message': message
       }
