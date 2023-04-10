331e3bf709c4ded1adbe736a5704a03ad0b1e84b
Sourav Kumar <souravk326@gmail.com>
2023-04-10 11:02:17 +0000

    created bucket

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbingServerlessCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucketname: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucket = this.createBucket(bucketname);
+  }
+  createBucket(bucketName: string): s3.IBucket{
+    const bucket = new s3.Bucket(this, 'Thumbingbucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
   }
 }
