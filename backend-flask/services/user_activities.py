4c046cc05a60557d1b9004991d001d36de6a64b1
Sourav Sourav <souravk.cloud@gmail.com>
2023-04-19 05:38:56 +0000

    remove the try and finally

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,29 +2,27 @@ from lib.db import db
 #from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    try:
-      model = {
-        'errors': None,
-        'data': None
-      }
-      
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        sql = db.template('users','show')
-        results = db.query_object_json(sql,{'handle': user_handle})
-        return results
-        model['data'] = results
-
-      # subsegment = xray_recorder.begin_subsegment('mock-data')
-      # # xray ---
-      # dict = {
-      #   "now": now.isoformat(),
-      #   "results-size": len(model['data'])
-      # }
-      # subsegment.put_metadata('key', dict, 'namespace')
-      # xray_recorder.end_subsegment()
-    finally:  
-    #  # Close the segment
-     # xray_recorder.end_subsegment()
-      return model
+    #try:
+    model = {
+      'errors': None,
+      'data': None
+    }
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
+      model['data'] = results
+      #subsegment = xray_recorder.begin_subsegment('mock-data')
+      ## xray ---
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
+      #xray_recorder.end_subsegment()
+    #finally:  
+    ##  # Close the segment
+    #  xray_recorder.end_subsegment()
+    return model
