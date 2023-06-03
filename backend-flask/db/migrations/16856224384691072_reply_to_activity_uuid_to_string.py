c4f10d604f28e031d5266b8dc63958adfeefe114
Sourav Kumar <souravk326@gmail.com>
2023-06-03 06:50:54 +0000

    added migration code

--- backend-flask/db/migrations/16856224384691072_reply_to_activity_uuid_to_string.py
+++ backend-flask/db/migrations/16856224384691072_reply_to_activity_uuid_to_string.py
@@ -3,10 +3,14 @@ from lib.db import db
 class ReplyToActivityUuidToStringMigration:
   def migrate_sql():
     data = """
+    ALTER TABLE activities DROP COLUMN reply_to_activity_uuid;
+    ALTER TABLE activities ADD COLUMN reply_to_activity_uuid uuid;
     """
     return data
   def rollback_sql():
     data = """
+    ALTER TABLE activities DROP COLUMN reply_to_activity_uuid;
+    ALTER TABLE activities ADD COLUMN reply_to_activity_uuid integer;
     """
     return data
 
