90c334e7cb097746191bfeec8464a2e412658b3e
Sourav Kumar <souravk326@gmail.com>
2023-06-05 10:57:53 +0000

    missing ,

--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -4,7 +4,7 @@ SELECT
   users.handle,
   activities.message,
   activities.expires_at,
-  activities.reply_to_activity_uuid
+  activities.reply_to_activity_uuid,
   activities.expires_at
 FROM public.activities
 INNER JOIN public.users ON users.uuid = activities.user_uuid 
